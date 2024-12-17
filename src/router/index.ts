f1cda5ff7b14435c09780af8577d9ddc7ee45160
aenderson23 <aenderson777@gmail.com>
2024-12-17 18:10:25 -0300

    Correção de bug 2.1.0

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,4 @@
 //imports
-import Cliente from "@/views/Cliente.vue";
-import AlterarCliente from "@/views/cliente/AlterarCliente.vue";
-import CadastrarCliente from "@/views/cliente/CadastrarCliente.vue";
-import Compra from "@/views/Compra.vue";
-import CadastrarProduto from "@/views/compra/CadastrarProduto.vue";
-import ComprarProduto from "@/views/compra/ComprarProduto.vue";
-import DeletarProduto from "@/views/compra/DeletarProduto.vue";
-import EditarProduto from "@/views/compra/EditarProduto.vue";
-import Vitrine from "@/views/compra/Vitrine.vue";
 import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
@@ -19,60 +10,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "home",
     component: Home,
-  },
-  //interface dos clientes, referenciado no store clientes
-  {
-    path: "/cliente",
-    name: "interface de clientes",
-    component: Cliente,
-    children: [
-      {
-        path: "",
-        name: "interface de cadastro de clientes",
-        component: CadastrarCliente,
-      },
-      {
-        path: ":id",
-        name: "editor de clientes",
-        component: AlterarCliente,
-        props:true
-      },
-    ],
-  },
-  //interface de compra, referenciado no store de compra
-  {
-    path: "/compra",
-    name: "interface de produtos",
-    component: Compra,
-    children: [
-      {
-        path: "",
-        name: "interface de vitrine de produtos",
-        component: Vitrine,
-      },
-      {
-        path: "adicionar",
-        name: "interface de cadastro de produtos",
-        component: CadastrarProduto,
-      },
-      {
-        path: "deletar",
-        name: "interface de deleção de produtos",
-        component: DeletarProduto,
-      },
-      {
-        path: "alterar/:id",
-        name: "interface de alteração de produtos",
-        component: EditarProduto,
-        props:true
-      },
-      {
-        path:"checkout/:id",
-        name: "interface de compra de produtos",
-        component: ComprarProduto,
-        props: true
-      }
-    ],
   },
   //interface de login e conexão com o bubble
   {
